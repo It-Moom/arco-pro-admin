8e8be4c95c9e593494f788dd1b4fb50c11837f35
倚窗听雨丿醉无心 <37876877+Zhang-Wei-666@users.noreply.github.com>
2024-05-23 00:08:43 +0800

    使用动态表单组件重写分布表单第一个步骤

--- src/pages/form/step/components/base-info.vue
+++ src/pages/form/step/components/base-info.vue
@@ -1,97 +1,26 @@
 <template>
-  <a-form
+  <AcroDynamicForm
     ref="formRef"
+    class="w-125!"
     :model="formData"
-    class="form"
-    :label-col-props="{ span: 6 }"
-    :wrapper-col-props="{ span: 18 }"
-  >
-    <a-form-item
-      field="activityName"
-      :label="$t('stepForm.form.label.activityName')"
-      :rules="[
-        {
-          required: true,
-          message: $t('stepForm.form.error.activityName.required'),
-        },
-        {
-          match: /^[a-zA-Z0-9\u4e00-\u9fa5]{1,20}$/,
-          message: $t('stepForm.form.error.activityName.pattern'),
-        },
-      ]"
-    >
-      <a-input
-        v-model="formData.activityName"
-        :placeholder="$t('stepForm.placeholder.activityName')"
-      />
-    </a-form-item>
-    <a-form-item
-      field="channelType"
-      :label="$t('stepForm.form.label.channelType')"
-      :rules="[
-        {
-          required: true,
-          message: $t('stepForm.form.error.channelType.required'),
-        },
-      ]"
-    >
-      <a-select
-        v-model="formData.channelType"
-        :placeholder="$t('stepForm.placeholder.channelType')"
-      >
-        <a-option>APP通用渠道</a-option>
-      </a-select>
-    </a-form-item>
-    <a-form-item
-      field="promotionTime"
-      :label="$t('stepForm.form.label.promotionTime')"
-      :rules="[
-        {
-          required: true,
-          message: $t('stepForm.form.error.promotionTime.required'),
-        },
-      ]"
-    >
-      <a-range-picker v-model="formData.promotionTime" />
-    </a-form-item>
-    <a-form-item
-      field="promoteLink"
-      :label="$t('stepForm.form.label.promoteLink')"
-      :rules="[
-        {
-          required: true,
-          message: $t('stepForm.form.error.promoteLink.required'),
-        },
-        {
-          type: 'url',
-          message: $t('stepForm.form.error.promoteLink.pattern'),
-        },
-      ]"
-      row-class="keep-margin"
-    >
-      <a-input
-        v-model="formData.promoteLink"
-        :placeholder="$t('stepForm.placeholder.promoteLink')"
-      />
-      <template #help>
-        <span>{{ $t('stepForm.form.tip.promoteLink') }}</span>
-      </template>
-    </a-form-item>
-    <a-form-item>
-      <a-button type="primary" @click="onNextClick">
-        {{ $t('stepForm.button.next') }}
-      </a-button>
-    </a-form-item>
-  </a-form>
+    :fields="fields"
+    :show-reset-button="false"
+    :label-col-props="{ span: 6 }" :wrapper-col-props="{ span: 18 }"
+    :submit-button-text="$t('stepForm.button.next')"
+    @submit="onNextClick"
+  />
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue';
-  import type { FormInstance } from '@arco-design/web-vue/es/form';
+  import type { AcroDynamicFormInstance } from '@mixte/snippets/acro-dynamic-form';
+  import { AcroDynamicForm, defineAcroDynamicFormFields } from '@mixte/snippets/acro-dynamic-form';
   import type { BaseInfoModel } from '@/apis/form';
 
   const emits = defineEmits(['changeStep']);
-  const formRef = ref<FormInstance>();
+
+  const { t } = useI18n();
+
+  const formRef = ref<AcroDynamicFormInstance>();
   const formData = ref<BaseInfoModel>({
     activityName: '',
     channelType: '',
@@ -99,38 +28,55 @@
     promoteLink: 'https://arco.design',
   });
 
+  const fields = defineAcroDynamicFormFields([
+    {
+      field: 'activityName',
+      label: t('stepForm.form.label.activityName'),
+      rules: [
+        { required: true, message: t('stepForm.form.error.activityName.required') },
+        { match: /^[a-zA-Z0-9\u4E00-\u9FA5]{1,20}$/, message: t('stepForm.form.error.activityName.pattern') },
+      ],
+      type: 'input',
+      componentProps: {
+        placeholder: t('stepForm.placeholder.activityName'),
+      },
+    },
+    {
+      field: 'channelType',
+      label: t('stepForm.form.label.channelType'),
+      rules: [{ required: true, message: t('stepForm.form.error.channelType.required') }],
+      type: 'select',
+      componentProps: {
+        placeholder: t('stepForm.placeholder.channelType'),
+        options: [{ label: 'APP通用渠道', value: 'APP通用渠道' }],
+      },
+    },
+    {
+      field: 'promotionTime',
+      label: t('stepForm.form.label.promotionTime'),
+      rules: [{ required: true, message: t('stepForm.form.error.promotionTime.required') }],
+      type: 'range-picker',
+    },
+    {
+      field: 'promoteLink',
+      label: t('stepForm.form.label.promoteLink'),
+      rules: [
+        { required: true, message: t('stepForm.form.error.promoteLink.required') },
+        { type: 'url', message: t('stepForm.form.error.promoteLink.pattern') },
+      ],
+      formItemProps: {
+        rowClass: 'mb-5!',
+        help: t('stepForm.form.tip.promoteLink'),
+      },
+      type: 'input',
+      componentProps: {
+        placeholder: t('stepForm.placeholder.promoteLink'),
+      },
+    },
+  ]);
+
   async function onNextClick() {
-    const res = await formRef.value?.validate();
-    if (!res)
+    if (!(await formRef.value?.validate()))
       emits('changeStep', 'forward', { ...formData.value });
   }
 </script>
-
-<style scoped lang="less">
-  .container {
-    padding: 20px;
-    .keep-margin {
-      margin-bottom: 20px;
-    }
-  }
-
-  .wrapper {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 64px 0;
-    background-color: var(--color-bg-2);
-  }
-
-  .steps {
-    margin-bottom: 36px;
-  }
-
-  .form {
-    width: 500px;
-  }
-
-  .form-content {
-    padding: 8px 50px 0 30px;
-  }
-</style>
