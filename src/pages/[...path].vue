02334a8f57f37d83ecf9cd0b3da9b6c87d5115b4
倚窗听雨丿醉无心 <994039348@qq.com>
2024-06-01 08:58:49 +0800

    修改 404 页面路径; 修复未找到的页面不会跳转 404 页面的问题

---
+++ src/pages/[...path].vue
@@ -0,0 +1,26 @@
+<template>
+  <div text-center pos="absolute top-1/2 left-1/2" ml--95px mt--121px>
+    <a-result class="result" status="404" subtitle="not found" />
+    <div class="operation-row">
+      <a-button key="back" type="primary" @click="back">back</a-button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useRouter } from 'vue-router';
+  import { DEFAULT_ROUTE_NAME } from '@/modules/router/const';
+
+  definePage({
+    name: 'NotFound',
+    meta: {
+      requiresAuth: false,
+    },
+  });
+
+  const router = useRouter();
+
+  function back() {
+    router.push({ name: DEFAULT_ROUTE_NAME });
+  }
+</script>
