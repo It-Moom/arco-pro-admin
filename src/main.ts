42184570b4eb56c0220902546d94c5b2a5031424
倚窗听雨丿醉无心 <994039348@qq.com>
2024-06-04 08:42:22 +0800

    移动 directive 中内容至 directives 文件夹中

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { createApp } from 'vue';
 import ArcoVueIcon from '@arco-design/web-vue/es/icon';
 
-import directive from './directive';
 import App from './App.vue';
+import directives from '@/directives';
 import router from '@/modules/router/index';
 import i18n from '@/modules/i18n/index';
 import pinia from '@/modules/pinia/index';
@@ -22,6 +22,6 @@ app.use(ArcoVueIcon);
 app.use(router);
 app.use(pinia);
 app.use(i18n);
-app.use(directive);
+app.use(directives);
 
 app.mount('#app');
